c60e9ec2f70e570883fcae328b3576f1bd74ccc3
Sergii Deshunin <deshunin@seas.upenn.edu>
2019-12-08 21:19:39 +0200

    Just two new lines added to the class Main

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("The first line created in the Project...");
+		System.out.println("Can't say it was so easy...");
+		System.out.println("End of the execution... Exit");
 
 	}
 
